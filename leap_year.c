80bc53b4a33eff1093c74248cf306365307bff98
jeshika311 <jeshikatha@gmail.com>
2024-10-17 16:47:20 +0530

    add new file

--- leap_year.c
+++ leap_year.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 int main(){
-    int x; // x is the no. of days in a year
+    int year; // year is the no. of days in a year
 
     printf("Enter year :-");  //taking input from user
-    scanf("%d" , &x);
+    scanf("%d" , &year);
 
-    if(x%4==0 )
+    if((year%400==0) || (year%4==0 && year%100!=0)){ 
         printf("It is leap year");
-    
-    else
+    }
+    else{
         printf("It is not a leap year");
+    }
     
     return 0;
 }
