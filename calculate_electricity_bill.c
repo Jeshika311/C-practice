0ef8ed6f72f83812e51a0da737469fe4786de112
jeshika311 <jeshikatha@gmail.com>
2024-10-18 11:41:59 +0530

    add new file

--- calculate_electricity_bill.c
+++ calculate_electricity_bill.c
@@ -1,26 +1,21 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main() {
     float units;
 
     printf("Enter Units: ");
-    scanf("%f" , &units);
+    scanf("%f", &units);
 
-    if(units<0)
-    {
+    if (units < 0) {
         printf("Invalid Input\n");
+    } else if (units <= 100) {
+        printf("Your electricity bill is: $0.00\n");
+    } else if (units <= 300) {
+        printf("Your electricity bill is: $%.2f\n", (units - 100) * 5.00);
+    } else {
+        printf("Your electricity bill is: $%.2f\n", (200 * 5.00) + ((units - 300) * 10.00));
     }
-    else if(units<=100)
-    {
-        printf("Your electricity bill is zero\n");
-    }
-    else if(units<=300)
-    {
-        printf("Your electricity bill is :-%f\n" , (units -100)*5.00);
-    }
-    else
-    {
-        printf("Your electricity bill is :-%f\n" , (200)*5.00 + (units-300)*10.00);
-    }
-    printf("___THANKYOU___\n");
+
+    printf("___THANK YOU___\n");
     return 0;
-};
+}
